953bbe2a33749007c817864e4571b68cdfd0dd15
chyalexcheng <chyalexcheng@gmail.com>
2022-07-29 15:23:27 +0200

    save all samples and steps in one file

--- data_processing/GetNetworkData.py
+++ data_processing/GetNetworkData.py
@@ -14,6 +14,8 @@ contact_keys = [
         'E', # young's modulus  = 10^E
         'v', # poisson's ratio
         'mu', # sliding friction
+        'sample', # sample ID
+        'step', # loadsep ID
         ]
 
 input_keys = [
@@ -22,11 +24,15 @@ input_keys = [
         'e_x',  # radial strain in x
         'e_y',  # radial strain in y
         'e_z',  # axial strain in z
+        'l_x',  # domain size in x
+        'l_y',  # domain size in y
+        'l_z',  # domain size in z
         'num',  # number of particles
 ]
 
 output_keys_bodies = [
         ## particle info (in b.shape and b.state)
+        'id',
         'shape.radius',
         'state.mass',
         'state.pos[0]',
@@ -86,10 +92,6 @@ unused_keys_constant = [
 TARGET_DIR = '/home/cheng/DataGen/'
 DATA_DIR = '/home/cheng/DataGen/'
 
-# select a strain stepID or sample ID and leave the other '*'
-sampleID = '200'
-stepID = '*'
-
 def main(pressure, experiment_type,numParticles):
     data_dir = DATA_DIR + f'{pressure}/{experiment_type}/{numParticles}/'
     if not os.listdir(data_dir):
@@ -97,27 +99,23 @@ def main(pressure, experiment_type,numParticles):
         return
 
     # get DEM state file names
-    simStateName = 'simState_' + experiment_type + f'_{sampleID}_{numParticles}'
-    file_names = glob.glob(data_dir + '/' + f'{simStateName}_{stepID}.yade.gz')
-
-    # recover the correct ordering of the files
-    if stepID == '*':
-        steps = sorted([int(f.split('.yade.gz')[0].split('_')[-1]) for f in file_names])
-        file_names = [data_dir + '/' + f'{simStateName}_{stepID}.yade.gz' for stepID in steps]
+    simStateName = 'simState_' + experiment_type + f'_*_{numParticles}_0.yade.gz'
+    file_names = glob.glob(data_dir + '/' + f'{simStateName}')
+    # get the list of sample IDs
+    samples = [int(f.split('.yade.gz')[0].split('_')[-3]) for f in file_names]
 
     # name the HDF5 file
-    if '*' in simStateName: h5file_name = data_dir + '/' + 'simState_' + experiment_type + f'_all_{stepID}_{numParticles}' + '_graphs.hdf5'
-    else: h5file_name = data_dir + '/' + simStateName + '_graphs.hdf5'
+    h5file_name = data_dir + '/' + 'simState_' + experiment_type + f'_all_{numParticles}' + '_graphsNew.hdf5'
     if os.path.exists(h5file_name):
         os.remove(h5file_name)
-    f_perstep = h5py.File(h5file_name, 'a')
+    f_all = h5py.File(h5file_name, 'a')
 
-    # load YADE and store data in f_perstep
-    for f in file_names:
+    # load YADE and store data in f_all
+    for sample in samples:
+        simStateName = data_dir + '/' + 'simState_' + experiment_type
+        # load the initial time step
         try:
-            O.load(f); O.step()
-            step = int(f.split('_')[-1].split('.yade.gz')[0])
-            sample = int(f.split('_')[2])
+            O.load(f'{simStateName}_{sample}_{numParticles}_0.yade.gz'); O.step()
         except IOError:
             print('IOError', f, pressure)
             continue
@@ -126,51 +124,65 @@ def main(pressure, experiment_type,numParticles):
             O.materials[0].young,
             O.materials[0].poisson,
             O.materials[0].frictionAngle,
-            sample,
-            step
             ])
-        ### input data (void ratio e, mean pressure, number of particles)
-        inputs_tensor = np.array([
-            porosity()/(1-porosity()),
-            getStress().trace()/3,
-            triax.strain[0],
-            triax.strain[1],
-            triax.strain[2],
-            len(O.bodies)
-            ])
-        ### output data
-        ## particle info
-        bodies_data = []
-        for bodyKey in output_keys_bodies:
-            bodies_data.append([float(eval('b.'+bodyKey)) for b in O.bodies])
-        bodies_data = np.array(bodies_data)
-        ## interaction info
-        inters_data = []
-        for interKey in output_keys_inters:
-            inters_data.append([float(eval('i.'+interKey)) for i in O.interactions if i.isReal])
-        inters_data = np.array(inters_data)
 
-        ## add to DGL library format (from Aron)
-        src = inters_data[0, :].astype(int)
-        dst = inters_data[1, :].astype(int)
-        e = inters_data[2:]
-        e = np.transpose(e, (1, 0))
-        n = np.transpose(bodies_data, (1, 0))
-        
-        if stepID   == '*': f_step = f_perstep.require_group(f'{step}')
-        if sampleID == '*': f_step = f_perstep.require_group(f'{sample}')
-        if stepID == '*' and sampleID == '*': f_step = f_perstep.require_group(f'{sample_step}')
+        # get DEM state file names
+        file_names = glob.glob(f'{simStateName}_{sample}_{numParticles}_*.yade.gz')
+        # get the list of loadstep IDs
+        steps = sorted([int(f.split('.yade.gz')[0].split('_')[-1]) for f in file_names])
 
-        f_step['contact_params'] = contact_tensor
-        f_step['sources'] = src
-        f_step['destinations'] = dst
-        f_step['edge_features'] = e
-        f_step['node_features'] = n
-        f_step['input_features'] = inputs_tensor
+        f_sample = f_all.create_group(f'{sample}')
+        f_sample['contact_params'] = contact_tensor
+        f_sample['num_steps'] = len(steps)
+
+        for step in steps:
+            # load YADE at a given time step
+            try:
+                O.load(f'{simStateName}_{sample}_{numParticles}_{step}.yade.gz'); O.step()
+            except IOError:
+                print('IOError', f, pressure)
+                continue
+            ### input data (void ratio e, mean pressure, number of particles)
+            inputs_tensor = np.array([
+                porosity()/(1-porosity()),
+                getStress().trace()/3,
+                triax.strain[0],
+                triax.strain[1],
+                triax.strain[2],
+                O.cell.hSize[0,0],
+                O.cell.hSize[1,1],
+                O.cell.hSize[2,2],
+                len(O.bodies)
+                ])
+            ### output data
+            ## particle info
+            bodies_data = []
+            for bodyKey in output_keys_bodies:
+                bodies_data.append([float(eval('b.'+bodyKey)) for b in O.bodies])
+            bodies_data = np.array(bodies_data)
+            ## interaction info
+            inters_data = []
+            for interKey in output_keys_inters:
+                inters_data.append([float(eval('i.'+interKey)) for i in O.interactions if i.isReal])
+            inters_data = np.array(inters_data)
+
+            ## add to DGL library format (from Aron)
+            src = inters_data[0, :].astype(int)
+            dst = inters_data[1, :].astype(int)
+            e = inters_data[2:]
+            e = np.transpose(e, (1, 0))
+            n = np.transpose(bodies_data, (1, 0))
+            
+            f_step = f_sample.create_group(f'{step}')
+            f_step['sources'] = src
+            f_step['destinations'] = dst
+            f_step['edge_features'] = e
+            f_step['node_features'] = n
+            f_step['input_features'] = inputs_tensor
 
     print(f'Added data to {h5file_name}')
 
 for pressure in ['0.1e6']:
     for experiment_type in ['drained']:
-        for numParticles in ['10000','15000']:
+        for numParticles in ['15000']:
             main(pressure, experiment_type, numParticles)
--- data_processing/readme.md
+++ data_processing/readme.md
@@ -6,7 +6,7 @@
 ## Data stored
 
 When importing the generated hdf5 file to python you will get a dictionary with the following keys:
-* `contact_params` : array of size 3 containing Young modulus $E$, poisson ratio $\nu$, and friction angle $\mu$ in degrees.
+* `contact_params` : array of size 5 containing Young modulus $E$, poisson ratio $\nu$, friction angle $\mu$ in degrees, sample ID (same as the seed), and loadstep ID
 * ints between 0 and 200: The key name is the index of the saved state. Each dictionary contains the following dictionaries with keys:
 1. `sources` : list of size (number_edges,) with the indices of the source node of each edge.
 2. `destinations` : list of size (number_edges,) with the indices of the destination node of each edge.
@@ -21,37 +21,31 @@ Table 1. Input parameter:
 |[2]| strain in x direction $\varepsilon_x$ |
 |[3]| strain in y direction $\varepsilon_y$ |
 |[4]| strain in z direction $\varepsilon_z$ |
-|[5]| number of particles|
+|[5]| domain size in x direction $ l_x$     |
+|[6]| domain size in x direction $ l_y$     |
+|[7]| domain size in x direction $ l_z$     |
+|[8]| number of particles|
 
 4. `node_features` : Particle features
 
 Table 2. Node parameters:
 |index |Node parameter|
 |:----:|:-------|
-| [0] | radius|
-| [1] | mass  |
-|[2] | position x coordinate|
-|[3] | position y coordinate|
-|[4] | position z coordinate|
-|[5] | velocity x component|
-|[6] | velocity y component|
-|[7] | velocity z component|
-|[8] | angular velocity 1 in YADE | 
-|[9] | angular velocity 2 in YADE | 
-|[10] | angular velocity 3 in YADE | 
-|[11] | angular momentum 1 in YADE|
-|[12] | angular momentum 2 in YADE|
-|[13] | angular momentum 3 in YADE|
-|[14] | Inertia 1 in YADE|
-|[15] | Inertia 2 in YADE|
-|[16] | Inertia 3 in YADE|
-|[17] | x coordinate at t=0, with respect to the origin|
-|[18] | y coordinate at t=0, with respect to the origin|
-|[19] | z coordinate at t=0, with respect to the origin|
-|[20] | orientation angle 1 in YADE at t=0, with respect to the origin|
-|[21] | orientation angle 2 in YADE at t=0, with respect to the origin|
-|[22] | orientation angle 3 in YADE at t=0, with respect to the origin|
-|[23] | orientation angle 4 in YADE at t=0, with respect to the origin|
+|[0] | ID    |
+|[1] | radius|
+|[2] | mass  |
+|[3] | position x coordinate|
+|[4] | position y coordinate|
+|[5] | position z coordinate|
+|[6] | velocity x component|
+|[7] | velocity y component|
+|[8] | velocity z component|
+|[9] | angular velocity 1 in YADE | 
+|[10] | angular velocity 2 in YADE | 
+|[11] | angular velocity 3 in YADE | 
+|[12] | x coordinate at t=0, with respect to the origin|
+|[13] | y coordinate at t=0, with respect to the origin|
+|[14] | z coordinate at t=0, with respect to the origin|
 
 5. `edge_features` : Contact features
 
@@ -72,21 +66,18 @@ Table 3. Edge parameters:
 |[11] |contactPoint x coordinate in the cross section of the overlap|
 |[12] |contactPoint y coordinate|
 |[13] |contactPoint z coordinate|
-|[14] |elastic component of the shear (tangential) force x (redundant equal to shearForce)|
-|[15] |elastic component of the shear (tangential) force y|
-|[16] |elastic component of the shear (tangential) force z|
-|[17]| elastic component of the shear displacement x|
-|[18]| elastic component of the shear displacement y|
-|[19]| elastic component of the shear displacement z|
-|[20]| total shear displacement x|
-|[21]| total shear displacement y|
-|[22]| total shear displacement z|
-|[23]| shear force x|
-|[24]| shear force y|
-|[25]| shear force z|
-|[26]| normal force x|
-|[27]| normal force y|
-|[28]| normal force z|
+|[14]| elastic component of the shear displacement x|
+|[15]| elastic component of the shear displacement y|
+|[16]| elastic component of the shear displacement z|
+|[17]| total shear displacement x|
+|[18]| total shear displacement y|
+|[19]| total shear displacement z|
+|[20]| shear force x|
+|[21]| shear force y|
+|[22]| shear force z|
+|[23]| normal force x|
+|[24]| normal force y|
+|[25]| normal force z|
 
 ## How to run it
 To run `GetNetworkData.py` you must have [yade](https://yade-dem.org/doc/installation.html) installed and then simply run `yade GetNetworkData.py`.
