fd6d7689c5f89945a9d165494094e2d66234e035
Luisa Orozco <l.orozco@esciencecenter.nl>
2022-12-02 16:49:08 +0100

    First try on a setup allowing restart of training.

--- dem_sim/training.py
+++ dem_sim/training.py
@@ -5,6 +5,7 @@ from torch.nn import MSELoss
 from dem_sim.utils import periodic_difference
 from dem_sim.data_types import Prediction, GraphData
 
+from itertools import islice
 import wandb
 
 def train(
@@ -12,22 +13,23 @@ def train(
         optimizer,
         loader,
         loss_function,
+        metric,
         device,
         epochs: int = 1,
+        start_epoch: int = 0,
+        start_step: int = 0,
+        total_loss: float = 0.0
     ):
     optimizer.zero_grad()
     simulator.train()
     simulator.to(device)
-
-    metric = VectorMetrics()
     losses = []
+    if start_step > 0: loader = islice(loader, start_step, None)
 
-    for epoch in range(epochs):
-        metric.reset()
-        total_loss = 0
-        for i, (graph_data, step) in enumerate(loader):
+    for epoch in range(start_epoch, epochs):
+        for i, (graph_data, step) in enumerate(loader, start = start_step):
             graph_data, step = _unbatch(graph_data, step)
-            graph_data = graph_data.copy_to(device)
+            graph_data = graph_data.copy_to(device) #check if it is necessary after pin_memory=True in DataLoader definition
 
             prediction = simulator(graph_data, step)
             loss = loss_function(prediction, graph_data, step)
@@ -35,7 +37,18 @@ def train(
             loss.backward()
             total_loss += loss.item()
             optimizer.step()
-            wandb.log({"loss": loss})
+
+            if i>start_step and i%10 == 0: #Save model checkpoint
+                wandb.log({"loss": loss}, step = i)
+                torch.save({
+                'epoch': epoch,
+                'step': i,
+                'model_state_dict': simulator.state_dict(), #check if this stores info recursevily (model).
+                'optimizer_state_dict': optimizer.state_dict(),
+                'metric_state_dict': metric.state_dict(),
+                'total_loss_epoch': total_loss
+                }, 'outputs/model.pth')
+                print(f"Saved checkpoint at epoch {epoch}, step {i} to outputs/model.pth")
             
         total_loss = total_loss / (i + 1)
         losses.append(total_loss)
@@ -43,8 +56,11 @@ def train(
         print(f"Loss after epoch {epoch}: {total_loss:.4E}...")
         print(metric)
 
-        wandb.log(metric.dict)
-        wandb.log({"loss_epoch": total_loss})
+        wandb.log({"loss_epoch": total_loss, "epoch": epoch})
+        wandb.log(metric.dict.update({"epoch" : epoch}))
+        start_step = 0 # Reseting variables
+        metric.reset()
+        total_loss = 0
 
     return losses
 
@@ -163,6 +179,10 @@ class VectorMetrics(nn.Module):
         self.stress += self._compute_mean_norm(prediction.stress, graph_data.stress[step])
         self.count += 1
 
+    def load_state_dict(self, state_dict: dict):
+        for attribute in state_dict:
+            setattr(self, attribute, state_dict[attribute])
+
     def __str__(self):
         output = "Mean vector norm differences: "
         for attribute in ['positions', 'velocities', 'angular_velocities', 'stress']:
@@ -170,6 +190,12 @@ class VectorMetrics(nn.Module):
             mean = getattr(self, attribute) / self.count
             output += f"\n{attribute}:" + tabs + f"{mean:.4E}"
         return output
+
+    def state_dict(self):
+        state_dict = dict()
+        for attribute in ['positions', 'velocities', 'angular_velocities', 'stress', 'count']:
+            state_dict[attribute] = getattr(self,attribute)
+        return state_dict
 
     @staticmethod
     def _compute_mean_norm(a, b):
--- train_one_step_wandb.py
+++ train_one_step_wandb.py
@@ -9,27 +9,28 @@ from dem_sim.datasets import SampleDataset, StepDataset
 from dem_sim.simulator import Simulator
 from dem_sim.generator import GraphGenerator
 from dem_sim.model import GNNModel, NaiveForecasting
-from dem_sim.training import train, DEMLoss
+from dem_sim.training import train, DEMLoss, VectorMetrics
 
-# External packags dependencies
-import wandb
+# External packages dependencies
+import wandb, os
 
 if __name__ == '__main__':
-    #----------- wandb configuration ----------------
+    #----------- wandb configuration
     config = dict (
       batch_size = 1,
+      epochs = 2,
       cutoff_distance_prefactor = 2.,
       architecture = "CNN",
       dataset_id = "path_sampling_5000",
-      infrastructure = "crib utwente",
-      device = "cuda"
+      infrastructure = "MacOS Luisa",
+      device = "cpu"
     )
 
-    wandb.init(project="GrainLearning_GNN_1",
-        entity="grainlearning-escience",
-        notes="tweak baseline",
-        tags=["baseline", "paper1"],
-        config=config)
+    wandb.init(project = "GrainLearning_GNN_1",
+        entity = "grainlearning-escience",
+        notes = "testing time performance",
+        tags = ["baseline", "paper1"],
+        config = config)
 
     #---------- Simulator configuration
     if config['infrastructure'] == "Snellius":
@@ -41,25 +42,52 @@ if __name__ == '__main__':
     elif config['infrastructure'] == "MacOS Aron":
         data_dir = '/Users/aronjansen/Documents/grainsData/raw/'
 
+    #---------- Data loading
+    device = torch.device(config['device'])
     filename = 'simState_path_sampling_5000_graphs_reformatted.hdf5'
     sample_dataset = SampleDataset(data_dir + filename)
     step_dataset = StepDataset(sample_dataset)
-    device = torch.device(config['device'])
+    g = torch.Generator()
+    g.manual_seed(0)
+    loader = DataLoader(step_dataset, batch_size = config['batch_size'],
+                        pin_memory = True, generator = g)
 
-    generator = GraphGenerator(cutoff_distance=config['cutoff_distance_prefactor'] * sample_dataset.max_particle_radius)
-    model = GNNModel(device=device)
+    #---------- Model creation
+    generator = GraphGenerator(cutoff_distance = config['cutoff_distance_prefactor'] * sample_dataset.max_particle_radius)
+    model = GNNModel(device = device)
     wandb.watch(model) #This enables log pytorch gradients
     model.to(device)
-    simulator = Simulator(model=model, graph_generator=generator)
+    simulator = Simulator(model = model, graph_generator = generator)
     simulator.to(device)
 
+    #---------- Optimizer and loss function initialization
     optimizer = Adam(simulator.parameters())
-    loader = DataLoader(step_dataset, batch_size=config['batch_size'], shuffle=True)
     loss_function = DEMLoss()
+    metric = VectorMetrics()
+
+    #---------- Loading a checkpoint
+    if not os.path.isdir("outputs"): os.mkdir("outputs")
+    start_step, start_epoch= 0, 0
+    previous_loss = 0.0
+    if os.path.isfile('outputs/model.pth'):
+        checkpoint = torch.load('outputs/model.pth')
+        simulator.load_state_dict(checkpoint['model_state_dict'])
+        print('Previously trained model weights state_dict loaded...')
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        print('Previously trained optimizer state_dict loaded...')
+        metric.load_state_dict(checkpoint['metric_state_dict'])
+        print('Previously trained metric state_dict loaded...')
+        start_epoch = checkpoint['epoch']
+        start_step = checkpoint['step']
+        print(f"Previously trained for {start_epoch} epochs, and {start_step} steps ...")
+        previous_loss = checkpoint['total_loss_epoch']
 
     #---------- Training
-    losses = train(simulator, optimizer, loader, loss_function, device)
-    #wandb.log({"losses": losses})
+    losses = train(simulator, optimizer, loader, loss_function, metric, device,
+                   epochs = config['epochs'],
+                   start_epoch = start_epoch,
+                   start_step = start_step,
+                   total_loss = previous_loss)
     
     #---------- Rollout
     """
